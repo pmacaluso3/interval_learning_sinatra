c47fed87cc33a4136481c9232b74118fa23d406e
Peter Macaluso <moxious.manoeuvre@gmail.com>
2016-12-05 09:00:39 -0800

    games/play -> games/show. a game displays cards but modifies guesses.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,11 @@
 get '/games/:game_id' do
+  binding.pry
   @game = Game.find(params[:game_id])
   @game.sync_guesses_with_deck
+  @cards = @game.cards
+  haml :'games/show'
+end
+
+put '/games/:game_id' do
+  binding.pry
 end
