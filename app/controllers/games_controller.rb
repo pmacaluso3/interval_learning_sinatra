139aa3146d319bb57246512a003895a2958aba20
Peter Macaluso <moxious.manoeuvre@gmail.com>
2016-12-07 08:34:26 -0800

    scope games routes to current_user

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,17 @@
 get '/games/:game_id' do
-  @game = Game.find(params[:game_id])
+  @game = current_user.games.find(params[:game_id])
   @game.sync_guesses_with_deck
   @cards = @game.deck.cards
   haml :'games/show'
 end
 
 put '/games/:game_id' do
+  game =  current_user.games.find(params[:game_id])
+  deck = game.deck
   params[:cards].each do |card_id, answer_hash|
-    guess = Guess.find_by(game_id: params[:game_id], card_id: card_id)
+    guess = game.guesses.find_by(card_id: card_id)
     guess.grade(answer_hash[:answer])
   end
-  game =  Game.find(params[:game_id])
-  deck = game.deck
   # TODO: decks/:deck_id/games should be a dashboard of decks i've played,
   # times they were played, and mastery levels (sum of times correct / total possible times correct)
   # PICKUP: redirect to decks/:deck_id/games/:game_id
