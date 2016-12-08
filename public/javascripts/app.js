43e1cda5af8fc87a567f9c067d7872a5bca8878d
Peter Macaluso <moxious.manoeuvre@gmail.com>
2016-12-07 16:42:36 -0800

    js wip: js card objs have state of hidden/current/previous/complete and an advance function moves it onto the next one. is this more complicated than it needs to be?

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,2 +1,42 @@
+var Card = function(cardDom) {
+  this.$cardDom = $(cardDom);
+  this.question = this.$cardDom.find('.card-quiz-question').text();
+  this.correctAnswer = this.$cardDom.data('answer');
+};
+
+Card.prototype.check = function(successCallback, failCallback) {
+  debugger //what's this
+  var testAnswer = this.$cardDom.find('.card-quiz-answer').val();
+  if (testAnswer === this.correctAnswer) {
+    successCallback();
+  } else {
+    failCallback();
+  }
+}
+
+Card.prototype.moveToCurrent = function() {
+  this.$cardDom.removeClass('cardQuizHidden');
+  this.$cardDom.removeClass('cardQuizPrevious');
+  this.$cardDom.add
+}
+
+Card.prototype.moveToPrevious = function() {
+
+}
+
+Card.prototype.hide = function() {
+
+}
+
+Card.prototype.advancePosition = function
+
 $(function() {
+  $('.next-question-button').on('click', function(event) {
+    event.preventDefault();
+    $card = $(event.delegateTarget).closest('.card-quiz');
+    answer = $card.data('answer');
+
+    debugger
+  })
+  // PICKUP
 })
